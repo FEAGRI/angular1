ae053ab75a43efc55af8c53a58ebe026bf25251f
suporte-intranet <suporte-intranet@feagri.unicamp.br>
2017-09-04 15:17:27 -0300

    aplicação html index, footer, header, menu, template-custom aula 66 e 67

--- gulpTasks/server.js
+++ gulpTasks/server.js
@@ -1,11 +1,20 @@
 //angular1/gulpTasks/server.js
 
 const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
 
 gulp.task('server',['watch'], function(){ //antes de executar o server, vai executar o watch primeiro depois continua aqui...
-
+  gulp.src('public').pipe(webserver({
+      livereload:true,
+      port: 3000,
+      open: true
+  }))
 })
 
 gulp.task('watch', function() {
-
+  watch('app/**/*.html', () => gulp.start('app.html'))
+  watch('app/**/*.css', () => gulp.start('app.css'))
+  watch('app/**/*.js', () => gulp.start('app.js'))
+  watch('app/**/*.*', () => gulp.start('app.assets'))
 })
