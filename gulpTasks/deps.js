4c5defe0a0d8f1acac32d3b08ea7d7fbd2b27d98
suporte-intranet <suporte-intranet@feagri.unicamp.br>
2017-09-05 10:38:02 -0300

    até a aula 70 routes.js dashboard e billingCycle

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -12,12 +12,12 @@ gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
 gulp.task('deps.js', function(request, response, next){
   gulp.src([
     'node_modules/angular/angular.min.js',
-    'node_modules/angular-ui-router/realease/angular-ui-router.min.js',
+    'node_modules/angular-ui-router/release/angular-ui-router.min.js',
     'node_modules/angular-animate/angular-animate.min.js',
     'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
     'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
     'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
-    'node_modules/admin-lte/plugins/slimScroll/jqueryslimscroll.min.js',
+    'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
     'node_modules/admin-lte/dist/js/app.min.js'
   ])
   .pipe(uglify()) //minificar os arquivos, tornado o arquivo menor possível
