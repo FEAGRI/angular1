c8cedee47986ae0fb0dfed79ead4399ef704d2bd
suporte-intranet <suporte-intranet@feagri.unicamp.br>
2017-09-06 16:45:02 -0300

    components, factores, dashboard.html, contentHeader

---
+++ app/commom/components/valueBox.js
@@ -0,0 +1,35 @@
+//
+
+angular.module('primeiraApp').component('valueBox', {
+  bindings: {
+    grid:'@',
+    colorClass:'@',
+    value:'@',
+    text:'@',
+    iconClass: '@',
+  },
+  controller: [
+    'gridSystem', // mesmo nome do gridSystemFactory.js
+    function(gridSystem){ // mesmo nome
+    // this.$onInit = () => { Arrow Function igual a linha abaixo
+    // ou
+    // this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
+       this.$onInit = function (){ // $onInit vai inicializar sempre que o bindings(lin4) estiverem inicializados
+         this.gridClasses = gridSystem.toCssClasses(this.grid)
+       }
+    }
+  ],
+  template:`
+  <div class="{{ $ctrl.gridClasses }}">
+      <div class="small-box {{ $ctrl.colorClass }}">
+          <div class="inner">
+              <h3>{{ $ctrl.value }}</h3>
+              <p>{{ $ctrl.text }}</p>
+          </div>
+          <div class="icon">
+            <i class="fa {{ $ctrl.iconClass }}"></i>
+          </div>
+      </div>
+  </div>
+  `
+})
